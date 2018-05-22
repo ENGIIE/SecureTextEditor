684d832ff7fa52dd4375cdc909b1085b66b4f44a
paho <nnamffoh.accs@gmail.com>
2018-05-22 11:52:57 +0200

    aes encryption/decryption solved

--- src/main/java/FXMLController.java
+++ src/main/java/FXMLController.java
@@ -14,153 +14,25 @@ import java.io.IOException;
 
 public class FXMLController {
 
-    //dir = new File("./src/main/java/savefiles/textfiles.xml");
-
     /**
      * Create FileManager Object
      */
     private static FileManager manager = new FileManager();
 
-
     // FXML File Elements
     @FXML
     private Button open;
-
+    @FXML
+    private MenuButton mbopen;
+    @FXML
+    private MenuButton mbsave;
     @FXML
     private Button newFile;
-
-    @FXML
-    private Button save;
-
     @FXML
     private TextArea textArea;
 
-    @FXML
-    private MenuItem misave;
-
-    @FXML
-    private MenuItem miaes;
-
-    @FXML
-    private MenuItem miopen;
-
-    @FXML
-    private MenuItem miopenaes;
-
 
     //FXML Action Events
-
-    /**
-     * ActionEvent Open Button
-     * @param event
-     * @throws IOException
-     */
-    @FXML
-    protected void handleOpenButtonAction(ActionEvent event) throws IOException {
-        System.out.println("Open");
-
-        //Creating FileChooser
-        FileChooser chooser = new FileChooser();
-
-        //Filter only Txt Files
-        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
-
-        //Show Open Dialog
-        File f = chooser.showOpenDialog(open.getScene().getWindow());
-
-        //Check if an Document was found
-        if (f != null) {
-
-            //Read filename into String and Set it
-            String filename = null;
-            int pos = f.getName().lastIndexOf(".");
-            if(pos != -1) {
-                filename = f.getName().substring(0, pos);
-            }
-            manager.setFileName(filename);
-
-            //Save CurrentPath of file
-            manager.setPath(f.getPath());
-
-
-            //Load fxml file in Parent root
-            Parent root = FXMLLoader.load(getClass().getResource("editor.fxml"));
-
-            //Get current Stage
-            Stage stage = (Stage) open.getScene().getWindow();
-
-            //Create Scene with root element
-            Scene scene = new Scene(root);
-
-            //Get textArea from editor scene and load Content in text Area
-            Node node = scene.lookup("#textArea");
-            TextArea txta = (TextArea) node;
-            txta.setText(manager.loadText(f));
-
-            //Set the new Scene on the Stage
-            stage.setScene(scene);
-
-        }
-
-
-    }
-
-
-    /**
-     * ActionEvent Open Button
-     * @param event
-     * @throws IOException
-     */
-    @FXML
-    protected void handleAESOpenButtonAction(ActionEvent event) throws IOException {
-        System.out.println("Open AES");
-
-        //Creating FileChooser
-        FileChooser chooser = new FileChooser();
-
-        //Filter only Txt Files
-        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
-
-        //Show Open Dialog
-        File f = chooser.showOpenDialog(open.getScene().getWindow());
-
-        //Check if an Document was found
-        if (f != null) {
-
-            //Read filename into String and Set it
-            String filename = null;
-            int pos = f.getName().lastIndexOf(".");
-            if(pos != -1) {
-                filename = f.getName().substring(0, pos);
-            }
-            manager.setFileName(filename);
-
-            //Save CurrentPath of file
-            manager.setPath(f.getPath());
-
-
-            //Load fxml file in Parent root
-            Parent root = FXMLLoader.load(getClass().getResource("editor.fxml"));
-
-            //Get current Stage
-            Stage stage = (Stage) open.getScene().getWindow();
-
-            //Create Scene with root element
-            Scene scene = new Scene(root);
-
-            //Get textArea from editor scene and load Content in text Area
-            Node node = scene.lookup("#textArea");
-            TextArea txta = (TextArea) node;
-            txta.setText(manager.aesDecryptText(f));
-
-            //Set the new Scene on the Stage
-            stage.setScene(scene);
-
-        }
-
-
-    }
-
 
     /**
      * ActionEvent NewFile Button
@@ -170,76 +42,141 @@ public class FXMLController {
     @FXML
     protected void handleNewFileButtonAction(ActionEvent event) throws IOException {
         System.out.println("New File");
-
         //Set boolean nFile true
         manager.setPath(null);
         manager.setFileName(null);
-
         //Load fxml file in Parent root
         Parent root = FXMLLoader.load(getClass().getResource("editor.fxml"));
-
         //Get current Stage
         Stage stage = (Stage) newFile.getScene().getWindow();
-
         //Create Scene with root element
         Scene scene = new Scene(root);
-
         //Get textArea from editor scene and clear it
         Node node = scene.lookup("#textArea");
         TextArea txta = (TextArea) node;
         txta.setText("");
-
         //Set the new Scene on the Stage
         stage.setScene(scene);
-
     }
 
 
     /**
-     * ActionEvent Save Button
+     * ActionEvent Normal Open Button
+     * @param event
+     * @throws IOException
+     */
+    @FXML
+    protected void handleOpenButtonAction(ActionEvent event) throws IOException {
+        System.out.println("Open");
+        //Creating FileChooser
+        FileChooser chooser = new FileChooser();
+        //Filter only Txt Files
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        //Show Open Dialog
+        File f = chooser.showOpenDialog(mbopen.getScene().getWindow());
+        //Check if an Document was found
+        if (f != null) {
+            //Read filename into String and Set it
+            String filename = null;
+            int pos = f.getName().lastIndexOf(".");
+            if(pos != -1) {
+                filename = f.getName().substring(0, pos);
+            }
+            manager.setFileName(filename);
+            //Save CurrentPath of file
+            manager.setPath(f.getPath());
+            //Load fxml file in Parent root
+            Parent root = FXMLLoader.load(getClass().getResource("editor.fxml"));
+            //Get current Stage
+            Stage stage = (Stage) mbopen.getScene().getWindow();
+            //Create Scene with root element
+            Scene scene = new Scene(root);
+            //Get textArea from editor scene and load Content in text Area
+            Node node = scene.lookup("#textArea");
+            TextArea txta = (TextArea) node;
+            txta.setText(manager.loadText(f));
+            //Set the new Scene on the Stage
+            stage.setScene(scene);
+        }
+    }
+
+
+    /**
+     * ActionEvent Normal Save Button
      * @param event
      * @throws IOException
      */
     @FXML
     protected void handleSaveButtonAction(ActionEvent event) throws IOException {
         System.out.println("Save");
-
         //If there is no currentPath
         if(manager.getPath()== null) {
             //Creating FileChooser
             FileChooser chooser = new FileChooser();
-
             //Filter only Txt Files
             chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
-
             //Show Save Dialog
-            File f = chooser.showSaveDialog(save.getScene().getWindow());
-
+            File f = chooser.showSaveDialog(mbsave.getScene().getWindow());
             //If file is saved
             if (f != null) {
-
                 //Save fileName
                 manager.setFileName(f.getName());
-
                 //Save currentPath
                 manager.setPath(f.getPath());
-
                 try {
                     //Save Document in Path
                     manager.saveText(textArea.getText());
-
                 } catch (IOException ex) {
                     System.out.println(ex.getMessage());
                 }
-
             }
         }else{
             //Save Document in Path
             manager.saveText(textArea.getText());
         }
-
-
     }
+
+
+    /**
+     * ActionEvent AES Open Button
+     * @param event
+     * @throws IOException
+     */
+    @FXML
+    protected void handleAESOpenButtonAction(ActionEvent event) throws IOException {
+        System.out.println("Open AES");
+        //Creating FileChooser
+        FileChooser chooser = new FileChooser();
+        //Filter only Txt Files
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        //Show Open Dialog
+        File f = chooser.showOpenDialog(mbopen.getScene().getWindow());
+        //Check if an Document was found
+        if (f != null) {
+            //Read filename into String and Set it
+            String filename = null;
+            int pos = f.getName().lastIndexOf(".");
+            if(pos != -1) {
+                filename = f.getName().substring(0, pos);
+            }
+            manager.setFileName(filename);
+            //Save CurrentPath of file
+            manager.setPath(f.getPath());
+            //Load fxml file in Parent root
+            Parent root = FXMLLoader.load(getClass().getResource("editor.fxml"));
+            //Get current Stage
+            Stage stage = (Stage) mbopen.getScene().getWindow();
+            //Create Scene with root element
+            Scene scene = new Scene(root);
+            //Get textArea from editor scene and load Content in text Area
+            Node node = scene.lookup("#textArea");
+            TextArea txta = (TextArea) node;
+            txta.setText(manager.aesDecryptText(f));
+            //Set the new Scene on the Stage
+            stage.setScene(scene);
+        }
+    }
+
 
     /**
      * ActionEvent AES Save Button
@@ -249,46 +186,31 @@ public class FXMLController {
     @FXML
     protected void handleAESSaveButtonAction(ActionEvent event) throws IOException {
         System.out.println("AES");
-
         //If there is no currentPath
         if(manager.getPath()== null) {
             //Creating FileChooser
             FileChooser chooser = new FileChooser();
-
             //Filter only Txt Files
             chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
-
             //Show Save Dialog
-            File f = chooser.showSaveDialog(save.getScene().getWindow());
-
+            File f = chooser.showSaveDialog(mbsave.getScene().getWindow());
             //If file is saved
             if (f != null) {
-
                 //Save fileName
                 manager.setFileName(f.getName());
-
                 //Save currentPath
                 manager.setPath(f.getPath());
-
                 try {
                     //Save Document in Path
                     manager.aesEncryptText(textArea.getText());
-
                 } catch (IOException ex) {
                     System.out.println(ex.getMessage());
                 }
-
             }
-
-
         }else{
             //Save Document in Path
             manager.aesEncryptText(textArea.getText());
         }
-
-
-
     }
-
 
 }
--- src/main/java/FileManager.java
+++ src/main/java/FileManager.java
@@ -1,13 +1,12 @@
 import chapter2.Utils;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Base64;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
-import java.nio.charset.Charset;
 import java.security.Provider;
 import java.security.Security;
-import java.util.Arrays;
 
 public class FileManager {
 
@@ -34,27 +33,16 @@ public class FileManager {
             0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
             0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17 };
 
-    //ctLength
-    int ctLength;
-
-    //cipherText
-    byte[] cipherText;
-
 
     /**
      * Constructor
      */
     public FileManager() {
-
-
         //add BouncyCastle Provider
         Security.addProvider(new BouncyCastleProvider());
         Provider provider = Security.getProvider("BC");
-
         fileWriter = null;
         fileReader = null;
-        System.out.println("Aufruf");
-
     }
 
 
@@ -64,102 +52,16 @@ public class FileManager {
      * @throws IOException
      */
     public void saveText (String text) throws IOException {
-
-
         try {
-            //Current Path has to be available
+            //currentPath is available
             if(currentPath != null) {
-                //fileWriter = new FileWriter("./src/main/java/savefiles/"+tempName+".txt");
-
-                //Create new Writer
+                //Create new FileWriter
                 fileWriter = new FileWriter(currentPath);
-
                 //Write Text into File
                 fileWriter.write(text);
-
                 //Close Writer
                 fileWriter.close();
             }
-
-
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
-
-    }
-
-
-    /**
-     * Using AES Encryption
-     * @param text
-     * @throws IOException
-     */
-    public void aesEncryptText (String text) throws IOException {
-
-
-        try {
-
-
-            //Text into byte array
-            byte[] input = text.getBytes();
-            System.out.println("input length: "+testinput.length);
-
-            //System.out.println(Arrays.toString(input));
-
-
-            //Creating new SecretKey
-            SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
-
-            //Instantiate Cipher
-            Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding", "BC");
-
-
-            System.out.println("input text : " + Utils.toHex(testinput));
-
-
-            // encryption pass
-
-            //Initiate Cipher init()
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-
-            //Create cipherText output array
-            cipherText = new byte[testinput.length];
-
-            //Block by block encryption from input into cipherText update()
-            ctLength = cipher.update(testinput, 0, testinput.length, cipherText, 0);
-
-            //Get ctLength doFinal()
-            ctLength += cipher.doFinal(cipherText, ctLength);
-            //cipher.doFinal(cipherText);
-
-            System.out.println("cipher text: " + Utils.toHex(cipherText) + " bytes: " + ctLength);
-
-
-            //cipherText to String
-            String cipherTextString = new String(cipherText);
-
-            System.out.println(cipherTextString);
-
-            System.out.println("String to bytes: "+Utils.toHex(cipherTextString.getBytes()));
-
-
-            //Writing encrypted text into file
-
-            //Current Path has to be available
-            if(currentPath != null) {
-                //fileWriter = new FileWriter("./src/main/java/savefiles/"+tempName+".txt");
-
-                //Create new Writer
-                fileWriter = new FileWriter(currentPath);
-
-                //Write Text into File
-                fileWriter.write(cipherTextString);
-
-                //Close Writer
-                fileWriter.close();
-            }
-
-
         }catch (Exception ex){
             ex.printStackTrace();
         }
@@ -173,38 +75,77 @@ public class FileManager {
      * @throws IOException
      */
     public  String loadText (File file) throws IOException {
-
         //Current Text
         String text = "";
-
         try {
-
             //Create new FileReader and BufferedReader
             fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-
             //Current row
             String row = "";
-
-
             //As long as there are still lines
             while( (row = bufferedReader.readLine()) != null )
             {
                 //Append row on String text
                 text+= row;
             }
-
             //Close FileReader
             fileReader.close();
         }
         catch (Exception ex){
             ex.printStackTrace();
         }
-
         System.out.println(text);
         return text;
     }
 
+
+    /**
+     * Using AES Encryption
+     * @param text
+     * @throws IOException
+     */
+    public void aesEncryptText (String text) throws IOException {
+        try {
+            System.out.println("Encrypt Text: " + text);
+
+            //Text into byte array
+            byte[] input = text.getBytes();
+            System.out.println("input text : " + Utils.toHex(input));
+            //Creating new SecretKey
+            SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
+            //Instantiate Cipher
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");
+
+            // encryption pass
+            //Initiate Cipher init()
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            //Create cipherText output array
+            byte[] cipherText = new byte[cipher.getOutputSize(input.length)];
+            //Block by block encryption from input into cipherText update()
+            int ctLength = cipher.update(input, 0, input.length, cipherText, 0);
+            //Get ctLength, doFinal()
+            ctLength += cipher.doFinal(cipherText, ctLength);
+            //cipherText to String
+            //Base64 encoding
+            System.out.println("cipher text: " + Utils.toHex(cipherText) + " bytes: " + ctLength);
+            String cipherTextString = Base64.toBase64String(cipherText);
+            System.out.println("Encrypt CipherTextString: " + cipherTextString);
+
+            //Writing encrypted text into file
+            //Current Path has to be available
+            if(currentPath != null) {
+                //Create new FileWriter
+                fileWriter = new FileWriter(currentPath);
+                //Write Text into File
+                fileWriter.write(cipherTextString);
+                //Close Writer
+                fileWriter.close();
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
 
 
     /**
@@ -214,68 +155,58 @@ public class FileManager {
      * @throws IOException
      */
     public String aesDecryptText (File file) throws IOException {
-
         //Current Text
         String text = "";
-
         try {
-
             //Create new FileReader and BufferedReader
             fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-
             //Current row
             String row = "";
-
-
             //As long as there are still lines
             while( (row = bufferedReader.readLine()) != null )
             {
                 //Append row on String text
                 text+= row;
             }
-
             //Close FileReader
             fileReader.close();
 
-
             //Creating new SecretKey
             SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
-
             //Instantiate Cipher
-            Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding", "BC");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");
 
             // decryption pass
-
+            System.out.println("Decrypt Text: " + text);
+            //Decode text into cipherTextRead input array
+            byte[] cipherText = Base64.decode(text.getBytes());
+            //Important for the right length
+            int ctLength = cipherText.length;
+            System.out.println("cipher text: " + Utils.toHex(cipherText) + " bytes: " + ctLength);
+            //Create plainText output array
             byte[] plainText = new byte[ctLength];
-
+            //Initialize Cipher
             cipher.init(Cipher.DECRYPT_MODE, key);
-
+            //Decrypt block by block into plainText update()
             int ptLength = cipher.update(cipherText, 0, ctLength, plainText, 0);
-
+            //Cipher doFinal()
             ptLength += cipher.doFinal(plainText, ptLength);
-
             System.out.println("plain text : " + Utils.toHex(plainText) + " bytes: " + ptLength);
-
+            //plainText array to String
             String plainTextString = new String(plainText);
-
+            System.out.println("Decrypt plainTextString : " + text);
             text = plainTextString;
-
+            System.out.println("Decrypt TextString : " + text);
         }
         catch (Exception ex){
             ex.printStackTrace();
         }
-
-        System.out.println(text);
         return text;
-
-
-
     }
 
 
     //Set and get currentPath
-
     public void setPath (String path) {
         currentPath = path;
     }
@@ -286,15 +217,8 @@ public class FileManager {
 
 
     ////Set and get tempName
+    public void setFileName (String filename) { tempName = filename; }
 
-    public void setFileName (String filename) {
-
-        tempName = filename;
-    }
-
-    public String getFileName () {
-
-        return tempName;
-    }
+    public String getFileName () { return tempName; }
 
 }
